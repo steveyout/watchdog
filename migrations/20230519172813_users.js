c4374d647164e56772bc500701d6f1f38efa6d33
youtsteve1@gmail.com <youtsteve1@gmail.com>
2023-05-19 20:40:12 +0300

    added util functions

---
+++ migrations/20230519172813_users.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns {Knex.SchemaBuilder}
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('users', function (table) {
+    table.increments('id');
+    table.string('first_name', 255).notNullable();
+    table.string('last_name', 255).notNullable();
+    table.string('username', 255).notNullable();
+    table.integer('tg_id').unique();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns {Knex.SchemaBuilder}
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('products').dropTable('users');
+};
